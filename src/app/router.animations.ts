53b6b68613029d02eed53378d1f7dc1aec802c5e
Ryan Emberling <itsalaidbacklife@gmail.com>
2019-01-23 14:33:30 -0500

    changed transition animation to fracTutor (lowered final state)

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -129,7 +129,7 @@ if (window.innerWidth <= 600) {
 
         query(':enter #fracTutorContainer', [
           style({ transform: 'translateY(150%)' }),
-          animate('1s ease-out', style({ transform: 'translate(0px, 12px)', position: 'relative'}))
+          animate('1s ease-out', style({ transform: 'translate(0px, 15px)', position: 'relative'}))
           ], {optional: true}),
         query(':enter #cuttleBotContainer', [
           style({ transform: 'translateY(300%)' }),
