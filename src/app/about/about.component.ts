4645956ccd6d4ed8197468524910e020d38a619a
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-08-17 15:05:20 -0400

    chore(about): remove unused ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,26 +15,6 @@ import { TextWithMetaData } from './highlighted-paragraph/highlighted-paragraph.
 	selector: 'app-about',
 	templateUrl: './about.component.html',
 	styleUrls: ['./about.component.css'],
-	animations: [
-		trigger('myInsertRemoveTrigger', [
-			state('off', style({
-				width: '0%'
-			})),
-			state('on', style({
-				width: '100%'
-			})),
-			transition('* <=> *', [
-				// query(':enter .bar-fill-container', 
-				// 	animate('1s ease-in-out'), {optional: true}
-				// ),
-				// query(':leave .bar-fill-container', 
-				// 	animate('1s 1s ease-in-out'), {optional: true}
-				// ),
-				query('@*', animateChild(), {optional: true})
-				]
-			)
-		]),
-	]
 })
 export class AboutComponent implements OnInit {
 	skillSetNames: Array<string>;
@@ -49,25 +29,10 @@ export class AboutComponent implements OnInit {
 	get smallScreen() {
 		return window.innerWidth < 600;
 	}
-	get skillSet() {
-		return this.skillSetNames[this.skillIndex];
-	}
-	get learningLabel() {
-		if (this.smallScreen || window.innerWidth < 1380) {
-			return "Pedagogy"
-		} else {
-			return "Learning Science"
-		}
-	}
 
-	// set skillSet(val) {
-	// 	this.skillIndex = val;
-	// }
 	constructor() { }
 
 	ngOnInit() {
-		this.skillIndex = 0;
-		this.skillSetNames = ["Technical Skills", "Learning Science Skills"];
 	}
 
 }
