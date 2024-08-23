ef9ce962970c90c465beaa40a36231418b0150f1
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-08-23 13:32:47 -0400

    chore: delete stale components and animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactComponent } from './contact/contact.component';
-import { DetailsDimensionalAnalysisComponent } from './portfolio/portfolio_pages/details-dimensional-analysis/details-dimensional-analysis.component';
-import { FracTutorComponent } from './portfolio/portfolio_pages/frac-tutor/frac-tutor.component';
-import { CuttleBotComponent } from './portfolio/portfolio_pages/cuttle-overview/cuttle-bot/cuttle-bot.component';
-import { SurgeryDetailsComponent } from './portfolio/portfolio_pages/surgery-details/surgery-details.component';
-import { EgiaComponent } from './portfolio/portfolio_pages/egia/egia.component';
-import { ProofCrafterComponent } from './portfolio/portfolio_pages/proof-crafter/proof-crafter.component';
 import { CuttleOverviewComponent } from './portfolio/portfolio_pages/cuttle-overview/cuttle-overview.component';
 import { AssistmentsComponent } from './portfolio/portfolio_pages/assistments/assistments.component';
-import { CuttleArenaComponent } from './portfolio/portfolio_pages/cuttle-overview/cuttle-arena/cuttle-arena.component';
-import { CuttleMLComponent } from './portfolio/portfolio_pages/cuttle-overview/cuttle-ml/cuttle-ml.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'about', pathMatch: 'full' },
@@ -22,16 +13,6 @@ const routes: Routes = [
 		{ path: 'assistments', component: AssistmentsComponent },
 		{ path: 'cuttle', component: CuttleOverviewComponent },
 	] },
-	{ path: 'contact', component: ContactComponent, data: {state: 'contact'} },
-	{ path: 'doseTutor', component: DetailsDimensionalAnalysisComponent, data: {state: 'portfolioDetails'} },
-	{ path: 'fracTutor', component: FracTutorComponent, data: {state: 'fracTutorDetails'}},
-	{ path: 'cuttleOverview', component: CuttleOverviewComponent, data: {state: 'cuttleOverview'}},
-	{ path: 'cuttleBot', component: CuttleBotComponent, data: {state: 'cuttleBotDetails'}},
-	{ path: 'cuttleML', component: CuttleMLComponent, data: {state: 'cuttleMLDetails'}},
-	{ path: 'cuttleArena', component: CuttleArenaComponent, data: {state: 'cuttleArenaDetails'}},
-	{ path: 'vascular-surgery', component:SurgeryDetailsComponent, data: {state: 'vascular-surgery'} },
-	{ path: 'intro-cs', component: EgiaComponent, data: {state: 'egia'} },
-	{ path: 'proofCrafter', component: ProofCrafterComponent, data: {state: 'proofCrafter'} }
 ]
 @NgModule({
 	imports: [ RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'}) ],
--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,25 +1,5 @@
 import {trigger, state, animate, style, group, animateChild, query, stagger, transition} from '@angular/animations';
-// import { ScreenSizeService } from './screen-size.service';
 
-// alert(window.innerWidth);
-// export const routerTransition = trigger('routerTransition', [
-// 	transition('* => *', [
-
-// 		query(':enter, :leave', [
-// 			style({position: 'absolute', width: '100%'})
-// 			], {optional: true}), //End query()
-// 		group([		
-// 			query(':enter .entryWrapper', stagger(400, [
-// 							style({opacity: 0}),
-// 							animate('1s ease-in-out', style({opacity: 1}))
-// 							]), {optional: true}), //End query()
-// 			query(':leave', [
-// 				style({transform: 'translateX(0%)'}),
-// 				animate('.5s ease-in-out', style({transform: 'translateX(-200%)', position: 'fixed'}))
-// 				], {optional: true}), //End quer()
-// 			]) //End group()
-// 		]) //End transition()
-// 	]); //End trigger()
 
 // Small Screen Transitions
 if (window.innerWidth <= 600) {
@@ -44,60 +24,15 @@ if (window.innerWidth <= 600) {
           animate('0.5s ease-out', style({ transform: 'translate(0px, -25px)', position: 'relative' }))
         ], { optional: true }),
 
-        query(':enter #contactContainer', [
-          style({ transform: 'translateX(100%)' }),
-          animate('.5s ease-out', style({ transform: 'translate(0px, -25px)', position: 'relative'}))
-          ], {optional: true}),
-
         query(':leave', [
           style({ transform: 'translateX(0%)' }),
           animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)', position: 'fixed' }))
         ], { optional: true }),
 
-        query(':enter #dimensionalAnalysisContainer, :enter #cuttleOverviewContainer', [
+        query(':enter #cuttleOverviewContainer', [
           style({ transform: 'translateY(100%)' }),
           animate('1s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
           ], {optional: true}),
-
-        query(':enter #fracTutorContainer', [
-          style({ transform: 'translateY(100%)' }),
-          animate('1.25s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
-          ], {optional: true}),
-
-        query(':enter #cuttleBotContainer', [
-          style({ transform: 'translateY(200%)' }),
-          animate('1.6s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
-          ], {optional: true}),
-
-        // query(':enter .entryWrapper', stagger(400, [
-        //   style({ transform: 'translateY(100%)'}),
-        //   animate('1s ease-in-out',
-        //     style({ transform: 'translateY(0%)', opacity: 1})
-        //     )
-        //   ]), {optional: true}) //End query for :enter .entryWrapper
-
-
-        query(':enter .entryWrapper', [
-          style({ transform: 'translateY(100%)'}),
-          animate('1s ease-in-out',
-            style({ transform: 'translateY(0%)', opacity: 1})
-            )
-          ], {optional: true}),
-
-
-        query(':enter .bar-fill-container', [
-          style({ width: '0%'}),
-          animate('1s ease-in-out',
-            style({ width: '100%'})
-            )
-          ], {optional: true}),
-
-        // query(':enter .entryWrapper', [
-        //   style({transform: 'translateY(100%)'}),
-        //   animate('.75s ease-in-out',
-        //     style({transform: 'translateY(0%)', opacity: 1})
-        //     )
-        //   ], {optional: true}) //End entryWrapper query()
       ]) //End group
     ])
   ]) // end trigger
@@ -134,61 +69,10 @@ if (window.innerWidth <= 600) {
           animate('0.5s ease-in-out', style({ transform: 'translateX(150%)', position: 'relative' }))
         ], { optional: true }),
 
-        query(':enter #dimensionalAnalysisContainer, :enter #cuttleOverviewContainer', [
-          style({ transform: 'translateY(110%)' }),
-          animate('1s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
-          ], {optional: true}),
-
         query(':enter #cuttleOverviewContainer', [
           style({ transform: 'translateY(110%)' }),
           animate('.65s ease-out', style({ transform: 'translate(0px, -22px)', position: 'relative'}))
           ], {optional: true}),
-
-        query(':enter #cuttleBotContainer', [
-          style({ transform: 'translateY(200%)' }),
-          animate('1.25s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
-          ], {optional: true}),
-
-        query(':enter #cuttleMLContainer, :enter #cuttleArenaContainer', [
-          style({ transform: 'translateY(150%)' }),
-          animate('1s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
-          ], {optional: true}),
-
-        query(':enter #fracTutorContainer', [
-          style({ transform: 'translateY(150%)' }),
-          animate('1s ease-out', style({ transform: 'translate(0px, 0px)', position: 'relative'}))
-          ], {optional: true}),
-        query(':enter #proofCrafterContainer', [
-          style({ transform: 'translateY(110%)' }),
-          animate('1s ease-out', style({ transform: 'translate(0px, 0%)', position: 'relative'}))
-          ], {optional: true}),
-        // query(':enter .entryWrapper', stagger(400, [
-        //   style({ transform: 'translateY(100%)'}),
-        //   animate('1s ease-in-out',
-        //     style({ transform: 'translateY(0%)', opacity: 1})
-        //     )
-        //   ]), {optional: true}) //End query for :enter .entryWrapper
-
-
-        query(':enter .entryWrapper', [
-          style({ transform: 'translateY(100%)'}),
-          animate('1s ease-in-out',
-            style({ transform: 'translateY(0%)', opacity: 1})
-            )
-          ], {optional: true}),
-
-        query(':enter .bar-fill-container', [
-          style({ width: '0%'}),
-          animate('1s ease-in-out',
-            style({ width: '100%'})
-            )
-          ], {optional: true}),
-        // query(':enter .entryWrapper', [
-        // 	style({transform: 'translateY(100%)'}),
-        // 	animate('.75s ease-in-out',
-        // 		style({transform: 'translateY(0%)', opacity: 1})
-        // 		)
-        // 	], {optional: true}) //End entryWrapper query()
       ]) //End group
     ])
   ]) // end trigger
