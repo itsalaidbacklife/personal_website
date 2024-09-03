bddf8dd4d355d11945f6fc3e412fc3f5eceba7bb
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-09-03 09:42:04 -0400

    feat(portfolio): add links to portfolio text

--- src/app/picture-and-explanation/picture-and-explanation.component.ts
+++ src/app/picture-and-explanation/picture-and-explanation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TextWithMetaData } from '../about/highlighted-paragraph/highlighted-paragraph.component';
 
 @Component({
   selector: 'app-picture-and-explanation',
@@ -8,11 +9,12 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PictureAndExplanationComponent implements OnInit {
 	@Input() title: string;
 	@Input() img: string;
-	@Input() text: string;
+	@Input() text: string | TextWithMetaData[];
 	@Input() text2: string;
 	@Input() link: string;
 	@Input() caption: string;
 	@Input() smallScreenCaption: string;
+
 	get footerVisibility() {
 		if (window.innerWidth > 1000) {
 			return 'visible';
@@ -27,6 +29,14 @@ export class PictureAndExplanationComponent implements OnInit {
 			return false;
 		}
 	}
+
+	get paragraph1(): TextWithMetaData[] {
+		if (typeof this.text === 'string') {
+			return [{str: this.text}];
+		}
+		return this.text;
+	}
+
 	constructor() { }
 	ngOnInit() {
 	}
